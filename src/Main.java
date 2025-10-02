26f12ad89286b71a382fec78aefe56b789462306
hwshin1 <hwshin5044@gmail.com>
2025-10-02 13:02:50 +0900

    두 수의 차 구하기

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(3,4));
+        System.out.println(solution.solution(5,1));
     }
 }
 
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 + num2;
+        int answer = num1 - num2;
         return answer;
     }
 }
