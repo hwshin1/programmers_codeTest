fe7ce9ae2fa31749f90ebf6402dddeff9f51dcec
hwshin1 <hwshin5044@gmail.com>
2025-10-04 21:04:42 +0900

    몫 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-//문제 설명
-//정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
-//제한사항
-//0 ≤ num1 ≤ 100
-//0 ≤ num2 ≤ 100
+//정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+// 제한사항
+// 0 < num1 ≤ 100
+// 0 < num2 ≤ 100
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(4,5));
+        System.out.println(solution.solution(4,2));
     }
 }
 
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 * num2;
+        int answer = num1 / num2;
         return answer;
     }
 }
