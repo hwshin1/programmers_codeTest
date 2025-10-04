8cc12cda69a041debae1c33215388d40e65d9440
hwshin1 <hwshin5044@gmail.com>
2025-10-04 21:10:16 +0900

    숫자 비교하기

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 // 문제 설명
-// 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
+// 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
 // 제한사항
-// 0 < num1 ≤ 100
-// 0 < num2 ≤ 100
+// 0 ≤ num1 ≤ 10,000
+// 0 ≤ num2 ≤ 10,000
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(30,50));
+        System.out.println(solution.solution(5,5));
     }
 }
 
 class Solution {
     public int solution(int num1, int num2) {
-        double ans = (double) num1 / num2;
-        int answer = (int) (ans * 1000);
-
-        return answer;
+        if (num1 == num2) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
