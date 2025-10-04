d3e2cbe137b9821aa9e0c8f9baf3bdd64d65deea
hwshin1 <hwshin5044@gmail.com>
2025-10-04 21:08:29 +0900

    두 수의 나눗셈

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-//정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+// 문제 설명
+// 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
 // 제한사항
 // 0 < num1 ≤ 100
 // 0 < num2 ≤ 100
@@ -6,13 +7,15 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(4,2));
+        System.out.println(solution.solution(30,50));
     }
 }
 
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 / num2;
+        double ans = (double) num1 / num2;
+        int answer = (int) (ans * 1000);
+
         return answer;
     }
 }
