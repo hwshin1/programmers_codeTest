e3b7ea6b733ce44d131c6abd85de5b00f763f806
hwshin1 <hwshin5044@gmail.com>
2025-10-06 21:41:32 +0900

    배열의 평균값

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 // 문제 설명
-// 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
 // 제한사항
-// 0 < num1 ≤ 100
-// 0 < num2 ≤ 100
+// 0 ≤ numbers의 원소 ≤ 1,000
+// 1 ≤ numbers의 길이 ≤ 100
+// 정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(10,4));
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(solution.solution(numbers));
     }
 }
 
 class Solution {
-    public int solution(int num1, int num2) {
-        int answer = num1 % num2;
+    public double solution(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        double answer = (double) sum / numbers.length;
         return answer;
     }
 }
