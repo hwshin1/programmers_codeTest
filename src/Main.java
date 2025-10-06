f45e9f2deaf5ef00535d6605702b6579a10351f8
hwshin1 <hwshin5044@gmail.com>
2025-10-06 21:44:02 +0900

    나이 출력

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
 // 문제 설명
-// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+// 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
+// 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요.
 // 제한사항
-// 0 ≤ numbers의 원소 ≤ 1,000
-// 1 ≤ numbers의 길이 ≤ 100
-// 정답의 소수 부분이 .0 또는 .5인 경우만 입력으로 주어집니다.
+// 0 < age ≤ 120
+// 나이는 태어난 연도에 1살이며 매년 1월 1일마다 1살씩 증가합니다.
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int numbers = 30;
         System.out.println(solution.solution(numbers));
     }
 }
 
 class Solution {
-    public double solution(int[] numbers) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-        }
-        double answer = (double) sum / numbers.length;
-        return answer;
+    public int solution(int age) {
+        int result = 2022 - (age - 1);
+        return result;
     }
 }
