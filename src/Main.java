d11f88761428023b246fe0c8baa8bd425e713394
hwshin1 <hwshin5044@gmail.com>
2025-10-12 19:28:32 +0900

    중앙값 구하기

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,30 @@
 // 문제 설명
-// 각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다.
-// 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
-// 예각 : 0 < angle < 90
-// 직각 : angle = 90
-// 둔각 : 90 < angle < 180
-// 평각 : angle = 180
+// 중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다.
+// 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다.
+// 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
 
 // 제한사항
-// 0 < angle ≤ 180
-// angle은 정수입니다.
+//array의 길이는 홀수입니다.
+//0 < array의 길이 < 100
+//-1,000 < array의 원소 < 1,000
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int numbers = 120;
-        System.out.println(solution.solution(numbers));
+        int[] a = {9, -1, 0};
+        System.out.println(solution.solution(a));
     }
 }
 
 class Solution {
-    public int solution(int angle) {
-        if (angle > 0 && angle < 90) {
-            return 1;
-        } else if (angle == 90) {
-            return 2;
-        } else if (angle > 90 && angle < 180) {
-            return 3;
-        } else {
-            return 4;
-        }
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        int count = array.length;
+        int middleCount = (count + 1) / 2;
+
+        int answer = array[middleCount - 1];
+        return answer;
     }
 }
