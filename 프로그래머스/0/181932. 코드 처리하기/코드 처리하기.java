f8d1dbc493735db1c773c866fca4bcca4a4d4e36
hwshin1 <99725699+hwshin1@users.noreply.github.com>
2025-12-22 21:45:09 +0900

    [level 0] Title: 코드 처리하기, Time: 338.53 ms, Memory: 374 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        
+        int mode = 0;
+        
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') {
+                mode = 1 - mode;
+            } else if (mode == 0 && i % 2 == 0) {
+                answer += code.charAt(i);
+            } else if (mode == 1 && i % 2 != 0) {
+                answer += code.charAt(i);
+            }
+        }
+        
+        if (answer.equals("")) {
+            answer = "EMPTY";
+        }
+        
+        return answer;
+    }
+}
